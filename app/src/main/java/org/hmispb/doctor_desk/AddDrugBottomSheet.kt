02ee4fd77b1fd906c3e50bde83a1784c904c0ea3
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-09-16 02:04:03 +0530

    search feature added

--- app/src/main/java/org/hmispb/doctor_desk/AddDrugBottomSheet.kt
+++ app/src/main/java/org/hmispb/doctor_desk/AddDrugBottomSheet.kt
@@ -1,10 +1,15 @@
 package org.hmispb.doctor_desk
 
+import android.app.Dialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import org.hmispb.doctor_desk.databinding.BottomsheetAddDrugBinding
@@ -26,7 +31,33 @@ class AddDrugBottomSheet(val data : Data,val prescriptionViewModel: Prescription
         for(drug in data.drugList) {
             drugList.add(drug.drugName)
         }
-        binding.spinnerDrugName.adapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,drugList)
+
+        binding.spinnerDrugName.setOnClickListener {
+            val dialog = Dialog(requireContext())
+            dialog.setContentView(R.layout.dialog_searchable_spinner_drug)
+            dialog.window?.setLayout(650,800)
+            dialog.show()
+            //Initiate and assign variable
+            val editText = dialog.findViewById<EditText>(R.id.edit_text)
+            val listView = dialog.findViewById<ListView>(R.id.list_view)
+            //Array Adapter init
+            val adapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,drugList)
+            listView.adapter = adapter
+            editText.addTextChangedListener(object: TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    adapter.filter.filter(p0)
+                }
+
+                override fun afterTextChanged(p0: Editable?) = Unit
+
+            })
+            listView.setOnItemClickListener { p0, p1, p2, p3 ->
+                binding.spinnerDrugName.text = adapter.getItem(p2)
+                dialog.dismiss()
+            }
+        }
 
         val dosageList = mutableListOf<String>()
         for(dosage in data.drugDose) {
@@ -45,15 +76,21 @@ class AddDrugBottomSheet(val data : Data,val prescriptionViewModel: Prescription
                 binding.days.error = ""
                 return@setOnClickListener
             }
-            val drugItem = DrugItem(
-                data.drugList[binding.spinnerDrugName.selectedItemPosition],
-                data.drugDose[binding.spinnerDosage.selectedItemPosition],
-                data.drugFrequency[binding.spinnerFrequency.selectedItemPosition],
-                Integer.parseInt(binding.days.text.toString()),
-                binding.instructions.text.toString()
-            )
+            val drugItem = data.drugList.find {
+                it.drugName == binding.spinnerDrugName.text
+            }?.let { it1 ->
+                DrugItem(
+                    it1,
+                    data.drugDose[binding.spinnerDosage.selectedItemPosition],
+                    data.drugFrequency[binding.spinnerFrequency.selectedItemPosition],
+                    Integer.parseInt(binding.days.text.toString()),
+                    binding.instructions.text.toString()
+                )
+            }
             val list = prescriptionViewModel.drugList.value ?: mutableListOf()
-            list.add(drugItem)
+            if (drugItem != null) {
+                list.add(drugItem)
+            }
             prescriptionViewModel.drugList.postValue(list)
             dismiss()
         }
--- app/src/main/java/org/hmispb/doctor_desk/AddTestBottomSheet.kt
+++ app/src/main/java/org/hmispb/doctor_desk/AddTestBottomSheet.kt
@@ -1,10 +1,15 @@
 package org.hmispb.doctor_desk
 
+import android.app.Dialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.hmispb.doctor_desk.databinding.BottomsheetAddTestBinding
 import org.hmispb.doctor_desk.model.Data
@@ -25,11 +30,38 @@ class AddTestBottomSheet(val data : Data, val prescriptionViewModel: Prescriptio
             testList
                 .add(test?.testName ?: "")
         }
-        binding.spinnerTest.adapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,testList)
+        binding.spinnerTest.setOnClickListener {
+            val dialog = Dialog(requireContext())
+            dialog.setContentView(R.layout.dialog_searchable_spinner)
+            dialog.window?.setLayout(650,800)
+            dialog.show()
+            //Initiate and assign variable
+            val editText = dialog.findViewById<EditText>(R.id.edit_text)
+            val listView = dialog.findViewById<ListView>(R.id.list_view)
+            //Array Adapter init
+            val adapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,testList)
+            listView.adapter = adapter
+            editText.addTextChangedListener(object: TextWatcher{
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    adapter.filter.filter(p0)
+                }
+
+                override fun afterTextChanged(p0: Editable?) = Unit
+
+            })
+            listView.setOnItemClickListener { p0, p1, p2, p3 ->
+                binding.spinnerTest.text = adapter.getItem(p2)
+                dialog.dismiss()
+            }
+        }
 
         binding.addTest.setOnClickListener {
             val tests = prescriptionViewModel.testList.value ?: mutableListOf()
-            tests.add(data.labTestName[binding.spinnerTest.selectedItemPosition] ?: LabTestName.nullLabTestName )
+            tests.add(data.labTestName.find {
+                it?.testName == binding.spinnerTest.text
+            } ?: LabTestName.nullLabTestName )
 
             prescriptionViewModel.testList.postValue(tests)
             dismiss()
